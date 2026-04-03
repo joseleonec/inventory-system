c0c04e33482a8260876aa2b74a516bddea2c1e98
joseleonec <joleonca@gmail.com>
2026-04-03 18:20:45 -0500

    Implement ProductController with GET endpoint for retrieving product by ID

--- src/main/java/com/myenterprise/inventory/infrastructure/adapters/input/http/ProductController.java
+++ src/main/java/com/myenterprise/inventory/infrastructure/adapters/input/http/ProductController.java
@@ -1,6 +1,33 @@
 package com.myenterprise.inventory.infrastructure.adapters.input.http;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.myenterprise.inventory.domain.dto.product.ProductDTO;
+import com.myenterprise.inventory.domain.ports.input.ProductInPort;
+import com.myenterprise.inventory.infrastructure.adapters.input.mappers.ProductInAdapterMapper;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
-    
+
+    private ProductInPort productPort;
+
+    private ProductInAdapterMapper productMapper;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> getProduct(@PathVariable Long id) {
+
+        var product = productPort.findById(id);
+
+        var dto = productMapper.toDTO(product);
+
+        return ResponseEntity.ok(dto);
+        
+    }
+
 }
