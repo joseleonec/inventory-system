0ae6b6d1f8ffa32daab06dd5ae8ce410d944c911
joseleonec <joleonca@gmail.com>
2026-04-11 20:21:08 -0500

    refactor: remove @Repository annotation from ProductRepository and ProductTransactionRepository, and StockRepository

--- src/main/java/com/myenterprise/inventory/infrastructure/adapters/output/persistence/repository/StockRepository.java
+++ src/main/java/com/myenterprise/inventory/infrastructure/adapters/output/persistence/repository/StockRepository.java
@@ -3,11 +3,9 @@ package com.myenterprise.inventory.infrastructure.adapters.output.persistence.re
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.myenterprise.inventory.infrastructure.adapters.output.persistence.entities.StockEntity;
 
-@Repository
 public interface StockRepository extends JpaRepository<StockEntity, Long> {
 
     Optional<StockEntity> findByProductId(Long productId);
